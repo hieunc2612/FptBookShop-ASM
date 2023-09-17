73f406ea877f4b25567f082928a424b7404306ea
Hieu <nguyenconghieu@gmail.com>
2023-09-17 23:46:04 +0700

    Thêm các tệp và thư mục không theo dõi

---
+++ FptBookShop-main/FptBookStore/Controllers/StoreController.cs
@@ -0,0 +1,62 @@
+﻿using FptBookStore.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FptBookStore.Controllers
+{
+    
+    [Authorize]
+    public class StoreController : Controller
+    {
+
+        private readonly FptBookStoreContext _context;
+
+        public StoreController(FptBookStoreContext context)
+        {
+            _context = context;
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Index(string searchString, string minPrice, string maxPrice)
+        {
+            var books = _context.Books.Select(b => b);
+            
+            if(!string.IsNullOrEmpty(searchString) ) 
+            {
+                books = books.Where(b => b.Title.Contains(searchString) || b.Author.Contains(searchString));
+            }
+
+            if(!string.IsNullOrEmpty(minPrice) )
+            {
+                var min = int.Parse(minPrice);
+                books = books.Where(b => b.Price >= min);
+            }
+
+            if (!string.IsNullOrEmpty(maxPrice))
+            {
+                var min = int.Parse(maxPrice);
+                books = books.Where(b => b.Price <= min);
+            }
+            return View(await books.ToListAsync());
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Books == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+    }
+}
